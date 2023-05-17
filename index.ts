dda3b2b27574c7922b93456ef36495f3e481ecc4
Kevin Amiri <amiri990.us@gmail.com>
2023-05-17 16:12:15 +0200

    add

--- index.ts
+++ index.ts
@@ -62,6 +62,7 @@ class XLSXManager {
 
     public async writeData(data: any[]): Promise<void> {
         try {
+
             const worksheet = XLSX.utils.json_to_sheet(data);
             const workbook = XLSX.utils.book_new();
 
@@ -75,96 +76,47 @@ class XLSXManager {
         }
     }
 
-    public async appendRow(rowData: any): Promise<void> {
+    public async appendDataToXLSX(newData: any) {
         try {
-            const jsonData = await this.readData();
-            jsonData.push(rowData);
+            // Read the existing workbook
+            const buffer = fs.readFileSync(this.filePath);
+            let workbook = XLSX.read(buffer, { type: 'buffer' });
+            const sheetName = workbook.SheetNames[0];
+            let worksheet = workbook.Sheets[sheetName];
 
-            await this.writeData(jsonData);
+            // Convert worksheet to JSON
+            let data = XLSX.utils.sheet_to_json(worksheet);
+
+            // Append new data
+            data.push(newData);
+
+            // Convert JSON to worksheet
+            let newWorksheet = XLSX.utils.json_to_sheet(data);
+
+            // Delete old worksheet
+            delete workbook.Sheets[sheetName];
+
+            // Append new worksheet
+            XLSX.utils.book_append_sheet(workbook, newWorksheet, sheetName);
+
+            // Write workbook to file
+            XLSX.writeFile(workbook, this.filePath);
+
+            console.log("Data appended successfully");
         } catch (error) {
-            console.error('Error appending row to the XLSX file:', error);
-            throw error;
-        } finally {
-            console.log('Append operation completed.');
+            console.error("Error appending data: ", error);
         }
     }
 
-    public async appendColumn(columnData: any): Promise<void> {
-        try {
-            const jsonData = await this.readData();
-            jsonData.forEach((row: { [x: string]: any; }, index: string | number) => {
-                row['newColumn'] = columnData[index] || null;
-            });
-
-            await this.writeData(jsonData);
-        } catch (error) {
-            console.error('Error appending column to the XLSX file:', error);
-            throw error;
-        } finally {
-            console.log('Append column operation completed.');
-        }
-    }
-    public async updateSecondColumn(updateFunction: (firstColumnValue: any) => any): Promise<void> {
-        try {
-            const jsonData = await this.readData();
-            const keys = Object.keys(jsonData[0]);
-
-            if (keys.length < 2) {
-                throw new Error('The data does not have at least two columns.');
-            }
-
-            jsonData.forEach((row: { [x: string]: any; }) => {
-                row[keys[1]] = updateFunction(row[keys[0]]);
-            });
-
-            await this.writeData(jsonData);
-        } catch (error) {
-            console.error('Error updating the second column in the XLSX file:', error);
-            throw error;
-        } finally {
-            console.log('Update operation completed.');
-        }
-    }
-
-    public async updateThirdColumn(updateFunction: (row: any) => any): Promise<void> {
-        try {
-            const jsonData = await this.readData();
-            const keys = Object.keys(jsonData[0]);
-
-            if (keys.length < 3) {
-                console.log('The data does not have at least three columns. Appending a new column...');
-                keys.push('total_tokens');
-            }
-
-            jsonData.forEach((row) => {
-                row[keys[2]] = updateFunction(row);
-            });
-
-            await this.writeData(jsonData);
-        } catch (error) {
-            console.error('Error updating the third column in the XLSX file:', error);
-            throw error;
-        } finally {
-            console.log('Update operation completed.');
-        }
-    }
 
 }
 
 
 const sendTranslationRequest = async (text: string) => {
-    // const completion = await openai.createCompletion({
-    //     model: "gpt-3.5-turbo",
-    //     max_tokens: 700,
-    //     temperature: 0,
-    //     prompt: text,
-
-    // });
 
     const response = await openai.createEdit({
         model: "text-davinci-edit-001",
         input: text,
-        temperature: 0.5,
         instruction: "Translate it into Swedish",
     });
     console.log("Starting to process " + text.substring(0.10))
@@ -174,8 +126,9 @@ const sendTranslationRequest = async (text: string) => {
     //         { role: "assistant", content: "Translate the following English text to French" },
     //         { role: "user", content: text }],
     // });
-    console.log({ total_token: response.data.usage.total_tokens })
-    return response.data.choices[0].text
+    // console.log({ total_token: response.data.usage.total_tokens })
+    // console.log(response.data)
+    return { content: response.data.choices[0].text, total_tokens: response.data.usage.total_tokens }
     // return chat.data.choices[0].message?.content;
 };
 
@@ -196,7 +149,7 @@ const handleTasks = async () => {
 
     // Split the data into batches of 10-20 rows
     type T = any; // Replace 'any' with the actual type of the elements in your 'data' array
-    const batchSize: number = 10; // Define your batchSize as needed
+    const batchSize: number = 20; // Define your batchSize as needed
     const batches: T[][] = [];
 
     while (data.length) {
@@ -205,42 +158,28 @@ const handleTasks = async () => {
 
     // Process each batch
     for (const batch of batches) {
-        let totalTokens = 0;
+        // Prepare all the promises for the batch
+        const promises = batch.map((row: any) => {
+            return retryWithExponentialBackoff(sendTranslationRequest, 1, 2, true, 10, ['RateLimitError'])(row.source_language)
+                .then((translation: any) => {
+                    row.target_language = translation.content;
+                    row.total_tokens = translation.total_tokens;
+                    // Update the row in the Excel file
+                    return xlsxManager.appendDataToXLSX(row);
+                });
+        });
 
-        // Process each row in the batch
-        for (let i = 0; i < batch.length; i++) {
-            const sourceTokens = calculateTokens(batch[i].source_language);
-
-            if (totalTokens + sourceTokens > 20000) {
-                // If the token limit is reached, wait for a minute and reset the total token count
-                await new Promise(resolve => setTimeout(resolve, 60000));
-                totalTokens = 0;
-            }
-
-            totalTokens += sourceTokens;
-
-            const translation = await retryWithExponentialBackoff(sendTranslationRequest, 1, 2, true, 10, ['RateLimitError'])(batch[i].source_language);
-            batch[i].target_language = translation;
-
-            const targetTokens = calculateTokens(translation);
-            totalTokens += targetTokens;
-
-            batch[i].total_tokens = sourceTokens + targetTokens;
-
-            // Update the row in the Excel file
-            await xlsxManager.appendRow(batch[i]);
-        }
+        // Wait for all the promises to resolve
+        await Promise.all(promises);
 
         // Update the second and third columns in the Excel file
-        await xlsxManager.updateSecondColumn((firstColumnValue: any) => firstColumnValue.target_language);
-        await xlsxManager.updateThirdColumn((row: any) => row.total_tokens);
+        // await xlsxManager.updateSecondColumn((firstColumnValue: any) => firstColumnValue.target_language);
+        // await xlsxManager.updateThirdColumn((row: any) => row.total_tokens);
 
         // Wait for a minute before processing the next batch
         await new Promise(resolve => setTimeout(resolve, 60000));
     }
 };
 
-handleTasks().catch(console.error);
+handleTasks()
 
-
-handleTasks().catch(console.error);
