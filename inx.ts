daa51b484f36850172354f4ad39d6c8513abadc6
Kevin Amiri <amiri990.us@gmail.com>
2023-05-25 16:58:53 +0200

    add

--- inx.ts
+++ inx.ts
@@ -7,8 +7,8 @@ async function main() {
 
 
     evaluateSheet('inputs.xlsx');
-    const data = await readData(1);
-    console.log(data);
+    // const data = await readData(1);
+    // console.log(data);
 
     // evaluateSheet('your-file-path.xlsx');
     // writeData(2, {name: 'John', age: 30});
